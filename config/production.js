e46a3801c4982acd94cc04737678e48d3c415f91
Thomas Burnett-Taylor <tmfksoft@gmail.com>
2020-12-14 00:46:15 +0000

    chore: Added ability to override command triger via env DISCORD_COMMAND_TRIGGER

--- config/production.js
+++ config/production.js
@@ -2,5 +2,5 @@ module.exports = {
     client_id: process.env.DISCORD_CLIENT_ID,
     permissions: process.env.DISCORD_PERMISSIONS,
     loginToken: process.env.DISCORD_LOGIN_TOKEN,
-    commandTrigger: "!",
+    commandTrigger: process.env.DISCORD_COMMAND_TRIGGER || "!",
 }
